8ca8661160b78ae14a42db83f87c3f74172eb019
Takahashi Shota <shohh6119@gmial.com>
2025-02-16 15:08:09 +0900

    Fix: create jar file

--- src/views/DrawPlayer.java
+++ src/views/DrawPlayer.java
@@ -7,8 +7,8 @@ import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 public class DrawPlayer extends JPanel{
-    Image img= Toolkit.getDefaultToolkit().getImage("A.jpg");
-    Image img_sans = Toolkit.getDefaultToolkit().getImage("B.jpg");
+    Image img= Toolkit.getDefaultToolkit().getImage("src/A.jpg");
+    Image img_sans = Toolkit.getDefaultToolkit().getImage("src/B.jpg");
     public void paintComponent(int playerX,int playerY,Graphics g){
         super.paintComponent(g);
         g.drawImage(img, playerX, playerY, this);
