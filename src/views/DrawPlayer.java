dbbd6746e79184dc63f98fddc272a683128ab017
Takahashi Shota <shohh6119@gmial.com>
2025-02-16 17:10:42 +0900

    Revert "[WIP]"
    This reverts commit 4e5dcd03afd1acc7c07937d6422f7a45024bbae8.

--- src/views/DrawPlayer.java
+++ src/views/DrawPlayer.java
@@ -1,35 +1,21 @@
 package views;
 
-import javax.imageio.ImageIO;
-import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.io.IOException;
+import java.awt.Toolkit;
 
-public class DrawPlayer extends JPanel {
-  private Image img;
-  private Image img_sans;
-
-  // コンストラクタで画像をロード
-  public DrawPlayer() {
-    try {
-      img = ImageIO.read(getClass().getResource("/views/A.jpg"));
-      img_sans = ImageIO.read(getClass().getResource("/views/B.jpg"));
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }catch (NullPointerException e) {
-      e.printStackTrace();
-      System.out.println("画像ファイルが JAR に含まれていないか、パスが間違っています。");
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+public class DrawPlayer extends JPanel{
+    Image img= Toolkit.getDefaultToolkit().getImage("src/A.jpg");
+    Image img_sans = Toolkit.getDefaultToolkit().getImage("src/B.jpg");
+    public void paintComponent(int playerX,int playerY,Graphics g){
+        super.paintComponent(g);
+        g.drawImage(img, playerX, playerY, this);
     }
-  }
-
-  public void paintComponent(int playerX, int playerY, Graphics g) {
-    super.paintComponent(g);
-    g.drawImage(img, playerX, playerY, this);
-  }
-
-  public void paintSans(int playerX, int playerY, Graphics g) {
-    super.paintComponent(g);
-    g.drawImage(img_sans, playerX, playerY, this);
-  }
+        
+    public void paintSans(int playerX,int playerY,Graphics g){
+            super.paintComponent(g);
+            g.drawImage(img_sans, playerX, playerY, this);
+        }
 }
